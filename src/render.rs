d6f02956e0d8be0601ff879bfd226a2cc07ef433
nikki-sh <139382119+nikki-sh@users.noreply.github.com>
2023-10-11 09:09:22 -0400

    being transitioning to different parser structure

--- src/render.rs
+++ src/render.rs
@@ -79,9 +79,137 @@ impl Backend for Chromium {
     }
 }
 
-pub struct Inhouse {}
+pub struct Inhouse<'a> {
+    markdown: &'a Vec<Event<'a>>,
+    position: usize,
+    document: PdfDocumentReference,
+    page: PdfPageIndex,
+    layer: PdfLayerReference,
+    width: f32,
+    height: f32,
+    font: Font,
+}
 
-impl Backend for Inhouse {
+impl<'a> Inhouse<'a> {
+    fn new(markdown: String, title: String) -> Inhouse<'a> {
+        let width = 209.9;
+        let height = 297.0;
+        let mut list_level = 0;
+
+        let (doc, page1, layer1) = PdfDocument::new(title, Mm(width), Mm(height), "Layer 1");
+
+        let mut font = Font::new(
+            "assets/fonts/Roboto-Regular.ttf",
+            "assets/fonts/Roboto-Bold.ttf",
+            "assets/fonts/Roboto-Italic.ttf",
+            "assets/fonts/Roboto-BoldItalic.ttf",
+            &doc,
+        );
+
+        let current_layer = doc.get_page(page1).get_layer(layer1);
+
+        current_layer.begin_text_section();
+
+        current_layer.set_font(&font.get(), font.regular_size);
+        current_layer.set_text_cursor(Mm(1.0), Mm(height - 7.0));
+        current_layer.set_line_height(font.regular_size);
+        // current_layer.set_word_spacing(font.calc_word_spacing());
+        // current_layer.set_character_spacing(font.regular_size);
+        current_layer.set_text_rendering_mode(TextRenderingMode::Fill);
+
+        Inhouse {
+            markdown: &pulldown_cmark::Parser::new(&markdown).collect(),
+            position: 0,
+            document: doc,
+            page: page1,
+            layer: current_layer,
+            width,
+            height,
+            font,
+        }
+    }
+
+    fn render_doc(&mut self) {
+        while !self.is_at_end() {
+            self.render();
+        }
+
+        self.layer.end_text_section();
+    }
+
+    fn render(&mut self) {
+        match self.consume() {
+            Event::Start(tag) => self.handle_start(tag),
+            Event::End(tag) => self.handle_end(tag),
+            Event::Text(text) => self.render_text(*text),
+            Event::Code(_) => todo!(),
+            Event::Html(_) => todo!(),
+            Event::FootnoteReference(_) => todo!(),
+            Event::SoftBreak => self.layer.add_line_break(),
+            Event::HardBreak => self.layer.add_line_break(),
+            Event::Rule => todo!(),
+            Event::TaskListMarker(_) => todo!(),
+        }
+    }
+
+    fn handle_start(&mut self, tag: &Tag) {
+        match tag {
+            Tag::Paragraph => todo!(),
+            Tag::Heading(_, _, _) => todo!(),
+            Tag::BlockQuote => todo!(),
+            Tag::CodeBlock(_) => todo!(),
+            Tag::List(_) => todo!(),
+            Tag::Item => todo!(),
+            Tag::FootnoteDefinition(_) => todo!(),
+            Tag::Table(_) => todo!(),
+            Tag::TableHead => todo!(),
+            Tag::TableRow => todo!(),
+            Tag::TableCell => todo!(),
+            Tag::Emphasis => todo!(),
+            Tag::Strong => todo!(),
+            Tag::Strikethrough => todo!(),
+            Tag::Link(_, _, _) => todo!(),
+            Tag::Image(_, _, _) => todo!(),
+        }
+    }
+
+    fn handle_end(&mut self, tag: &Tag) {
+        match tag {
+            Tag::Paragraph => todo!(),
+            Tag::Heading(_, _, _) => todo!(),
+            Tag::BlockQuote => todo!(),
+            Tag::CodeBlock(_) => todo!(),
+            Tag::List(_) => todo!(),
+            Tag::Item => todo!(),
+            Tag::FootnoteDefinition(_) => todo!(),
+            Tag::Table(_) => todo!(),
+            Tag::TableHead => todo!(),
+            Tag::TableRow => todo!(),
+            Tag::TableCell => todo!(),
+            Tag::Emphasis => todo!(),
+            Tag::Strong => todo!(),
+            Tag::Strikethrough => todo!(),
+            Tag::Link(_, _, _) => todo!(),
+            Tag::Image(_, _, _) => todo!(),
+        }
+    }
+
+    fn render_text(&mut self, text: CowStr) {}
+
+    fn load(&mut self) {}
+
+    fn consume(&mut self) -> &Event {
+        let event = self.markdown.get(self.position).unwrap();
+        self.position += 1;
+        event
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.position >= self.markdown.len()
+    }
+}
+
+impl Backend for Inhouse<'_> {
     fn render_files(
         files: &HashMap<PathBuf, String>,
         template: String,
@@ -90,63 +218,16 @@ impl Backend for Inhouse {
         let mut rendered_files: HashMap<PathBuf, Vec<u8>> = HashMap::new();
 
         for (path, content) in files {
-            let width = 209.9;
-            let height = 297.0;
-
-            let (doc, page1, layer1) = PdfDocument::new(
-                path.clone()
-                    .with_extension("")
-                    .file_name()
-                    .unwrap()
-                    .to_str()
-                    .unwrap(),
-                Mm(209.9),
-                Mm(297.0),
-                "Layer 1",
-            );
-
-            let mut font = Font::new(
-                "assets/fonts/Roboto-Regular.ttf",
-                "assets/fonts/Roboto-Bold.ttf",
-                "assets/fonts/Roboto-Italic.ttf",
-                "assets/fonts/Roboto-BoldItalic.ttf",
-                &doc,
-            );
-
-            let current_layer = doc.get_page(page1).get_layer(layer1);
-
-            current_layer.begin_text_section();
-
-            // setup the general fonts.
-            // see the docs for these functions for details
-            current_layer.set_font(&font.get(), 24.0);
-            current_layer.set_text_cursor(Mm(1.0), Mm(height - 7.0));
-            current_layer.set_line_height(12.0);
-            current_layer.set_word_spacing(3000.0);
-            current_layer.set_character_spacing(4.0);
-            current_layer.set_text_rendering_mode(TextRenderingMode::Fill);
-
-            let parser = pulldown_cmark::Parser::new(content);
+            let parser: Vec<Event> = pulldown_cmark::Parser::new(content).collect();
 
             for event in parser {
                 match event {
                     Event::Start(start_event) => match start_event {
                         Tag::Paragraph => font.current_size = font.regular_size,
-                        Tag::Heading(depth, _, _) => {
-                            font.current_size = font.header_size
-                                - (font.header_size_scale_increment
-                                    * match depth {
-                                        HeadingLevel::H1 => 1.0,
-                                        HeadingLevel::H2 => 2.0,
-                                        HeadingLevel::H3 => 3.0,
-                                        HeadingLevel::H4 => 4.0,
-                                        HeadingLevel::H5 => 5.0,
-                                        HeadingLevel::H6 => 6.0,
-                                    })
-                        }
+                        Tag::Heading(depth, _, _) => font.start_header(depth),
                         Tag::BlockQuote => todo!(),
                         Tag::CodeBlock(_) => todo!(),
-                        Tag::List(_) => todo!(),
+                        Tag::List(_) => list_level += 1,
                         Tag::Item => todo!(),
                         Tag::FootnoteDefinition(_) => todo!(),
                         Tag::Table(_) => todo!(),
@@ -160,11 +241,14 @@ impl Backend for Inhouse {
                         Tag::Image(_, _, _) => todo!(),
                     },
                     Event::End(end_event) => match end_event {
-                        Tag::Paragraph => current_layer.add_line_break(),
+                        Tag::Paragraph => {
+                            current_layer.add_line_break();
+                            current_layer.add_line_break();
+                        }
                         Tag::Heading(depth, _, _) => current_layer.add_line_break(),
                         Tag::BlockQuote => todo!(),
                         Tag::CodeBlock(_) => todo!(),
-                        Tag::List(_) => todo!(),
+                        Tag::List(_) => list_level -= 1,
                         Tag::Item => todo!(),
                         Tag::FootnoteDefinition(_) => todo!(),
                         Tag::Table(_) => todo!(),
@@ -179,7 +263,10 @@ impl Backend for Inhouse {
                     },
                     Event::Text(text) => {
                         current_layer.set_font(&font.get(), font.current_size);
-                        current_layer.write_text(text.to_string(), &font.get());
+                        current_layer.write_text(
+                            format!("{}{}", " -> ".repeat(list_level), text.to_string()),
+                            &font.get(),
+                        );
                     }
                     Event::Code(_) => todo!(),
                     Event::Html(_) => todo!(),
@@ -246,7 +333,7 @@ impl Font {
             current_size: 8.0,
             regular_size: 8.0,
             header_size: 14.0,
-            header_size_scale_increment: 1.0,
+            header_size_scale_increment: 2.0,
         }
     }
 
@@ -258,6 +345,32 @@ impl Font {
             (false, true) => self.italic.clone(),
             (false, false) => self.regular.clone(),
         }
+    }
+
+    pub fn start_header(&mut self, depth: HeadingLevel) {
+        self.is_bold = true;
+        self.current_size = self.header_size
+            - (self.header_size_scale_increment
+                * match depth {
+                    HeadingLevel::H1 => 1.0,
+                    HeadingLevel::H2 => 2.0,
+                    HeadingLevel::H3 => 3.0,
+                    HeadingLevel::H4 => 4.0,
+                    HeadingLevel::H5 => 5.0,
+                    HeadingLevel::H6 => 6.0,
+                });
+    }
+
+    pub fn calc_character_spacing(&self) -> f32 {
+        0.0
+    }
+
+    pub fn calc_word_spacing(&self) -> f32 {
+        0.0
+    }
+
+    pub fn calc_line_height(&self) -> f32 {
+        0.0
     }
 }
 
